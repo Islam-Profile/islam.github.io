630c705e8afd86fb8e529571d93a28b31f86e692
islam98 <islamomar98@yahoo.com>
2021-11-14 22:18:49 +0200

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="header2">
+                <h1>Software Engineer</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Header
